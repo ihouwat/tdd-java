4bbf326ca3ee05974415b44b7d534378ead9c1f8
Igor <houwat@gmail.com>
2022-02-19 15:20:43 -0500

    add fizzBuzz method and tests

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class FizzBuzz {
 
     public FizzBuzz() {
@@ -5,6 +8,27 @@ public class FizzBuzz {
 
     public String sayHello() {
         return "FizzBuzz says hello!";
+    }
+
+    public List<String> fizzBuzz(int num) {
+        List<String> result = new ArrayList<>();
+        for(int i = 1; i <= num; i++) {
+            String str = "";
+            boolean divisibleBy3 = (i % 3 == 0);
+            boolean divisibleBy5 = (i % 5 == 0);
+
+            if(divisibleBy3) {
+                str += "Fizz";
+            }
+            if(divisibleBy5) {
+                str += "Buzz";
+            }
+            if(str.equals("")) {
+                str = Integer.toString(i);
+            }
+            result.add(str);
+        }
+        return result;
     }
 
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,12 +1,79 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.Mock;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
 
 class FizzBuzzTest {
 
-    private final FizzBuzz target = new FizzBuzz();
+    @Mock
+    private final FizzBuzz mock = mock(FizzBuzz.class);
+    private final FizzBuzz testClass = new FizzBuzz();
 
     @Test
-    void fizzBuzzMethodRuns() {
-        assertEquals("FizzBuzz says hello!", target.sayHello());
+    void test_saysHello_MethodReturnsCorrectString() {
+        assertEquals("FizzBuzz says hello!", testClass.sayHello());
     }
+
+    @Test
+    void test_saysHelloMethodInvokedOnce() {
+        mock.sayHello();
+        verify(mock, times(1)).sayHello();
+    }
+
+    @Test
+    void test_saysHello_MethodInvokedMultipleTimes() {
+        mock.sayHello();
+        mock.sayHello();
+        verify(mock, times(2)).sayHello();
+    }
+
+    @Test
+    void test_saysHello_MethodDoesNotInvoked() {
+        verify(mock, never()).sayHello();
+    }
+
+    @Test
+    void fizzBuzzMethodInvokedOnce() {
+        mock.fizzBuzz(anyInt());
+        verify(mock).fizzBuzz(anyInt());
+    }
+
+    @Test
+    void fizzBuzzReturnsAListOfIntegersOnly() {
+        List<String> result = testClass.fizzBuzz(2);
+        String[] testResult = {"1", "2"};
+        assertArrayEquals(testResult, result.toArray());
+    }
+
+    @Test
+    void fizzBuzzReturnsOneInstanceOfFizz() {
+        List<String> result = testClass.fizzBuzz(3);
+        String[] testResult = {"1", "2", "Fizz"};
+        assertArrayEquals(testResult, result.toArray());
+    }
+
+    @Test
+    void fizzBuzzReturnsOneInstanceOfBuzz() {
+        List<String> result = testClass.fizzBuzz(5);
+        String[] testResult = {"1", "2", "Fizz", "4", "Buzz"};
+        assertArrayEquals(testResult, result.toArray());
+    }
+
+    @Test
+    void testResultExpectedNumberFiveButMethodReturnedBuzz() {
+        List<String> result = testClass.fizzBuzz(5);
+        String[] testResult = {"1", "2", "Fizz", "4", "5"};
+        assertFalse(Arrays.equals(testResult, result.toArray()));
+    }
+
+    @Test
+    void fizzBuzzReturnsOneInstanceOfFizzBuzz() {
+        List<String> result = testClass.fizzBuzz(15);
+        String[] testResult = {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"};
+        assertArrayEquals(testResult, result.toArray());
+    }
+
 }
